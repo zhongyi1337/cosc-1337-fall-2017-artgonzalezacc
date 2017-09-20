99a266223202d94601a73e6b2c91f4ad72896e78
zhongyi1337 <31940663+zhongyi1337@users.noreply.github.com>
2017-09-20 18:34:19 -0500

    Commit changes

--- src/Payroll.h
+++ src/Payroll.h
@@ -1,9 +1,18 @@
 #ifndef _PAYROLL_H
 #define _PAYROLL_H
 
-double getOvertimeHours(double totalHours);
-double getRegularHours(double totalHours);
-double getGrossPay(double regularHours, double overtimeHours, double hourlyRate);
-double getGrossPay(double salary);
+#include <string>
+using namespace std;
+double ot(double hours);
+double regularpay(double hours, double pay_rate);
+double otpay(double pay_rate, double ot);
+double grosspay(double regularpay, double otpay);
+double grosspay(double salary);
+double getFIT(double grosspay);
+double getFICASSN(double grosspay);
+double getFICAMED(double grosspay);
+double getNetpay(double grosspay, double fit, double ssn, double med);
+string contoStr(double db);
+void printout(string results[]);
 
 #endif
