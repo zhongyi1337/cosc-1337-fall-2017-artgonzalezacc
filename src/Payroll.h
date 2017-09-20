9754bb3a2c4f47c4dc5d2146bdab3611a72775e5
Art Gonzalez <arturo.gonzalez@austincc.edu>
2017-09-20 08:35:07 -0500

    add function prototypes

--- src/Payroll.h
+++ src/Payroll.h
@@ -1,7 +1,9 @@
 #ifndef _PAYROLL_H
 #define _PAYROLL_H
 
-
-
+double getOvertimeHours(double totalHours);
+double getRegularHours(double totalHours);
+double getGrossPay(double regularHours, double overtimeHours, double hourlyRate);
+double getGrossPay(double salary);
 
 #endif
